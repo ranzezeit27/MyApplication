eaf0b4a5de407e7a6fb2ee79c38495bfcb20649b
Ranzezeit <92981879+Ranzezeit@users.noreply.github.com>
2024-10-31 18:42:36 +0000

    change views.py

--- my_app/hello/views.py
+++ my_app/hello/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def show(request):
     return HttpResponse('Hello world haha');
+
+def not_show(request):
+    return HttpResponse("Not show");
